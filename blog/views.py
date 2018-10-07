ee200dbc24b8a3c8a3d09cce9d69b397fba2c9af
sohel2178 <sohel.ahmed2178@gmail.com>
2018-10-07 19:19:09 +0600

    Tofar Commit GhM

--- blog/views.py
+++ blog/views.py
@@ -1,36 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-# Some Dummy Post
-
-posts=[
-    {
-        'author':'Sohel Ahmed',
-        'title':'This is Post 1',
-        'content':'Post 1 Content',
-        'date_posted':'12 Aug 2018'
-    },
-    {
-        'author':'Sohel Ahmed',
-        'title':'This is Post 2',
-        'content':'Post 2 Content',
-        'date_posted':'12 Aug 2018'
-    },
-    {
-        'author':'Shakil Ahmed',
-        'title':'This is Post 1 From Shakil',
-        'content':'Post 1 Content of Shakil',
-        'date_posted':'12 Aug 2018'
-    }
-]
 
 # Create your views here.
 
 def home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html',{'title':'About'})
---
+++ users/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import RegistrationForm
+
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Account Created For {username}!')
+            return redirect('blog-home')
+    else:
+        form = RegistrationForm()
+
+    return render(request,'users/register.html',{'form':form})
+
