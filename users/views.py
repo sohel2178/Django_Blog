e1b03970b71cb52ad6a3544e2193e52c32a0d224
Sohel <sohel.ahmed2178@gmail.com>
2018-10-07 23:20:13 +0600

    HHHHHHHHHHHHHHHHHHHHHHHH

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import RegistrationForm
 
@@ -10,10 +11,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Account Created For {username}!')
-            return redirect('blog-home')
+            messages.success(request,f'Your account has been created. You are now able to log in !')
+            return redirect('login')
     else:
         form = RegistrationForm()
 
     return render(request,'users/register.html',{'form':form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
 
