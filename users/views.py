41113e99423b4aa1f292e28e234d9727f018d66a
Sohel <sohel.ahmed2178@gmail.com>
2018-10-12 21:54:24 +0600

    Update Profile Ses

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import RegistrationForm
+from .forms import RegistrationForm,UserUpdateForm,profileUpdateForm
 
 # Create your views here.
 
@@ -20,5 +20,21 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method =='POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = profileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request,f'Your Profile is Updated')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = profileUpdateForm(instance=request.user.profile)
+
+
+    context = {'u_form':u_form,'p_form':p_form}
+
+    return render(request,'users/profile.html',context)
 
